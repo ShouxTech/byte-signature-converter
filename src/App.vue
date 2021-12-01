c0a5689671574738cc12f41d23baad9238529875
ShouxTech <70824139+ShouxTech@users.noreply.github.com>
2021-12-01 18:38:00 -0500

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<template>
+	<h2>Convert between IDA styled byte signatures and code styled byte signatures</h2>
+	<p>{{result}}</p>
+	<TextBox placeholder="IDA Style Signature" @submitted="convertIDASignature"/>
+	<p>OR</p>
+	<TextBox ref="codeSig" style="margin-bottom: 10px;" placeholder="Code Style Signature" @submitted="convertCodeSignature"/>
+	<TextBox ref="codeMask" placeholder="Code Style Mask" @submitted="convertCodeSignature"/>
+</template>
+
+<script>
+import TextBox from './components/TextBox.vue'
+
+export default {
+	name: 'App',
+	components: {
+		TextBox
+	},
+	data() {
+		return {
+			result: '',
+			idaStyleUnknownByte: '?',
+			codeStyleUnknownByte: '00'
+		}
+	},
+	methods: {
+		convertIDASignature(signature) {
+			const bytes = signature.split(' ');
+
+			if (bytes[bytes.length - 1] == '') { // If there is a space at the end of the signature, remove it.
+				bytes.pop();
+			}
+
+			const codeSignature = [];
+			const codeSignatureMask = [];
+
+			for (const byte of bytes) {
+				if (byte == this.idaStyleUnknownByte) {
+					codeSignature.push(`\\x${this.codeStyleUnknownByte}`);
+					codeSignatureMask.push('?');
+				} else {
+					codeSignature.push(`\\x${byte}`);
+					codeSignatureMask.push('x');
+				}
+			}
+
+			this.result = `${codeSignature.join('')} | ${codeSignatureMask.join('')}`;
+		},
+
+		convertCodeSignature() {
+			const bytes = this.$refs.codeSig.$refs.textbox.value.split('\\x');
+			const mask = this.$refs.codeMask.$refs.textbox.value.split('');
+
+			bytes.shift(); // Remove first empty result.
+			console.log(bytes, mask);
+
+			const idaSignature = [];
+
+			for (const [index, byte] of bytes.entries()) {
+				if (mask[index] == '?') {
+					idaSignature.push(this.idaStyleUnknownByte);
+				} else {
+					idaSignature.push(byte);
+				}
+			}
+
+			this.result = idaSignature.join(' ');
+		}
+	}
+}
+</script>
+
+<style>
+body {
+	background-color: rgb(25, 25, 25);
+	color: rgb(255, 255, 255);
+    font-family: 'Ubuntu', sans-serif;
+}
+
+#app {
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+}
+
+h2 {
+	margin-bottom: 30px;
+}
+</style>
---
+++ src/components/TextBox.vue
@@ -0,0 +1,35 @@
+<template>
+    <input ref="textbox" type="text" :placeholder="placeholder">
+</template>
+
+<script>
+export default {
+    name: 'TextBox',
+    props: [
+        'placeholder'
+    ],
+    mounted() {
+        const textbox = this.$refs.textbox;
+
+        textbox.addEventListener('keyup', (event) => {
+            if (event.key == 'Enter') {
+                this.$emit('submitted', textbox.value);
+            }
+        });
+    }
+}
+</script>
+
+<style scoped>
+    input {
+        background-color: rgb(40, 40, 40);
+        border: none;
+        border-radius: 3px;
+        width: 50%;
+        height: 35px;
+        text-align: center;
+        color: rgb(255, 255, 255);
+        font-size: 17px;
+        font-family: 'Ubuntu', sans-serif;
+    }
+</style>
